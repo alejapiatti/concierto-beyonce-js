ce13b33f3b80ae91692911321cd6df0171dc7b33
Ale <Aleu@gmail.com>
2022-04-27 08:16:25 +0200

    Byonce

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Tu código va aquí: 
+let edad>= 16;
+{
+console.log("Eres mayor de edad, puedes entrar")
+}else if (edad >=14 && edad <=17){
+    console.log("Puedes entrar,pero sólo si te acompaña una persona mayor de edad")
+} else (edad<14){
+    console.log("Lo siento, eres demasiado joven, vuelve a intentarlo en unos años.)
+}
